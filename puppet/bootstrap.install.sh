cbd04485e929635eacea71ab0a2f1b8f023ef608
root <root@debian>
2014-07-04 13:19:01 +0200

    oprava

--- puppet/bootstrap.install.sh
+++ puppet/bootstrap.install.sh
@@ -3,11 +3,11 @@ apt-get install -y git puppet
 
 if [ ! -d /puppet ]; then
 	cd /
-	git clone https://home.zcu.cz/~bodik/meta/git/rsyslog2
+	git clone http://home.zcu.cz/~bodik/meta/git/rsyslog2
 	ln -sf /rsyslog2/puppet /puppet
 else
 	cd /puppet
-	git remote set-url origin https://home.zcu.cz/~bodik/meta/git/rsyslog2
+	git remote set-url origin http://home.zcu.cz/~bodik/meta/git/rsyslog2
 	git pull
 fi
 
