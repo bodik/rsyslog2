ccf4fc4f17a9c70339e65dd23d29695fc02389a9
root <root@debian>
2014-07-04 17:29:53 +0200

    upravy a import

--- puppet/rsyslog-dev.pp
+++ puppet/rsyslog-dev.pp
@@ -4,20 +4,18 @@ package { ["dpkg-dev", "gcc", "make", "fakeroot"]:
 	ensure => installed,
 }
 
-file { "/etc/apt/sources.list.d/jessie.list":
-        source => "/puppet/templates/etc/apt/sources.list.d/jessie.list",
+file { "/etc/apt/sources.list.d/wheezy-backports.list":
+        source => "/puppet/templates/etc/apt/sources.list.d/wheezy-backports.list",
         owner => "root", group => "root", mode => "0644",
         notify => Exec["apt-get update"],
 }
 
-
-
-#jessie v7.6 build deps
+# v7.6 build deps
 package { 
 	[ "debhelper", "dh-autoreconf", "dh-systemd", "zlib1g-dev", "libmysqlclient-dev", "libpq-dev", "libmongo-client-dev", 
-	"libcurl4-gnutls-dev", "libkrb5-dev", "libgnutls28-dev", "librelp-dev", "libestr-dev", "libee-dev",
+	"libcurl4-gnutls-dev", "libkrb5-dev", "libgnutls-dev", "librelp-dev", "libestr-dev", "libee-dev",
  	"liblognorm-dev", "liblogging-stdlog-dev", "libjson-c-dev", "uuid-dev", "pkg-config", "bison",
-	"libgcrypt20-dev", "flex"]:
+	"libgcrypt-dev", "flex"]:
 	ensure => installed,
 }
 
