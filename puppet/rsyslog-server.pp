f9fc936c4aaeade925f645dfbbf41b3a02563556
root <root@debian.localdomain>
2014-07-04 16:48:06 +0000

    typo v unless

--- puppet/rsyslog-server.pp
+++ puppet/rsyslog-server.pp
@@ -15,7 +15,7 @@ package { ["rsyslog", "rsyslog-gssapi"]:
 exec { "install_rsyslog_wheezy-backports":
 	command => "/usr/bin/apt-get install -q -y -o DPkg::Options::=--force-confold  -t wheezy-backports rsyslog rsyslog-gssapi",
 	timeout => 600,
-	unless => "/usr/bin/dpkg -l rsyslog | grep ' 7\.[0-9]'",
+	unless => "/usr/bin/dpkg -l rsyslog | grep ' 7\\.[0-9]'",
 	require => [File["/etc/apt/sources.list.d/wheezy-backports.list"], Package["rsyslog"], Package["rsyslog-gssapi"]],
 }
 
