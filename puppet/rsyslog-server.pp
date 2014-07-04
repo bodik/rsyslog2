bf99bdbb81d889e8463f78b6dda5f6523361a79c
root <root@debian>
2014-07-04 18:51:03 +0200

    pridani zavislosti

--- puppet/rsyslog-server.pp
+++ puppet/rsyslog-server.pp
@@ -16,6 +16,6 @@ exec { "install_rsyslog_wheezy-backports":
 	command => "/usr/bin/apt-get install -q -y -o DPkg::Options::=--force-confold  -t wheezy-backports rsyslog rsyslog-gssapi",
 	timeout => 600,
 	unless => "/usr/bin/dpkg -l rsyslog | grep ' 7\\.[0-9]'",
-	require => [File["/etc/apt/sources.list.d/wheezy-backports.list"], Package["rsyslog"], Package["rsyslog-gssapi"]],
+	require => [File["/etc/apt/sources.list.d/wheezy-backports.list"], Exec["apt-get update"], Package["rsyslog"], Package["rsyslog-gssapi"]],
 }
 
