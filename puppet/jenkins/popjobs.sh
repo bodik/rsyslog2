3f93865ed64db54ace1570d0fd0e7811f1a8abad
root <root@debian>
2014-07-06 03:37:41 +0200

    vyvoj

--- puppet/jenkins/popjobs.sh
+++ puppet/jenkins/popjobs.sh
@@ -1,3 +1,8 @@
+echo "INFO: deleting old config"
+for all in $(find jobs -mindepth 1 -maxdepth 1 -type d); do
+	rm -r $all
+done
+
 for all in `ls /var/lib/jenkins/jobs`; do
 	echo $all
 	mkdir jobs/$all
