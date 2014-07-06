71e6b6a2694545cdd9bd29e0125a43ac47dafe20
root <root@debian>
2014-07-06 17:50:44 +0200

    vyvoj

--- test02/testi.sh
+++ test02/testi.sh
@@ -8,9 +8,9 @@ else
 	TESTID=$2
 fi
 
-i=0
-while [ $i -lt $1 ]; do
-        logger "$TESTID tmsg$i"
+I=0
+while [ $I -lt $1 ]; do
+        logger "$TESTID tmsg$I"
 	#/rsyslog2/usleep 500
 	I=$(($I+1))
 done
