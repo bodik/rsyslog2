628abd13215384862d62ba8cee09e5cf76937fcd
root <root@dhcp17-231.ics.muni.cz>
2014-07-10 12:49:20 +0200

    vyvoj a oprava testu

--- puppet/tests/test_elk.sh
+++ puppet/tests/test_elk.sh
@@ -24,16 +24,30 @@ if [ $? -ne 0 ]; then
 fi
 
 
+
+
+
+
+
+
 #TODO: TOTO BY SE MELO PRESTEHOVAT AZ SE ROZTRHNE ES A LOGSTASH
 /usr/lib/nagios/plugins/check_procs --argument-array=/opt/logstash/lib/logstash/runner.rb -c 1:1
 if [ $? -ne 0 ]; then
 	rreturn 1 "$0 /opt/logstash/lib/logstash/runner.rb check_procs"
 fi
 
+LSLAGE=$(ps h -o etimes $(pgrep -f /opt/logstash/lib/logstash/runner.rb))
+if [ $LSLAGE -lt 60 ] ; then
+	sleep 60
+fi
+
 netstat -nlpa | grep " $(pgrep -f /opt/logstash/lib/logstash/runner.rb)/java" | egrep "(LISTEN|ESTABLISHED)"
 if [ $? -ne 0 ]; then
 	rreturn 1 "$0 /opt/logstash/lib/logstash/runner.rb not connected anywhere"
 fi
+
+
+
 
 
 
@@ -47,6 +61,8 @@ wget "http://$(facter fqdn)" -q -O - | grep "<title>Kibana 3" 1>/dev/null
 if [ $? -ne 0 ]; then
 	rreturn 1 "$0 kibana not found on webserver"
 fi
+
+
 
 
 
