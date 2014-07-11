2e1b0162fccfbbbd388da5ecf2d5c62e7afbb5d6
root <root@debian>
2014-07-11 17:30:24 +0200

    explicitni instalace avahi kvuli rezoluci rediseru

--- puppet/rsyslog-server.check.sh
+++ puppet/rsyslog-server.check.sh
@@ -3,7 +3,7 @@ if [ $? -eq 0 ]; then
         echo "INFO: RSYSLOGSERVERCHECK ======================="
 
 	export FACTER_rediser_server=$(avahi-browse -t _rediser._tcp --resolve -p | grep "=;.*;IPv4;" | awk -F";" '{print $8}' | xargs host -t A | rev | awk '{print $1}' | rev | sed 's/\.$//')
-        for all in rsyslog-server; do
+        for all in avahi rsyslog-server; do
                 echo "INFO: puppet apply -v --noop --show_diff $all.pp"
                 puppet apply -v --noop --show_diff $all.pp
         done
