00475f94f58d83fab29bd9d08cb997d96b8ff632
root <root@debian>
2014-07-13 18:55:12 +0200

    presunuti jenkinse

--- puppet/jenkins.check.sh
+++ puppet/jenkins.check.sh
@@ -1,7 +1,6 @@
 if [ -f /etc/default/jenkins ]; then
         echo "INFO: JENKINSCHECK ======================="
 
-	cd /puppet/jenkins || exit 1
         for all in jenkins; do
                 echo "INFO: puppet apply -v --noop --show_diff $all.pp"
                 puppet apply -v --noop --show_diff $all.pp
--- puppet/jenkins.install.sh
+++ puppet/jenkins.install.sh
@@ -1,3 +1,3 @@
 wget -q -O - http://pkg.jenkins-ci.org/debian-stable/jenkins-ci.org.key | apt-key add -a
-(cd /puppet && sh phase2.install.sh)
+sh phase2.install.sh
 puppet apply jenkins.pp
