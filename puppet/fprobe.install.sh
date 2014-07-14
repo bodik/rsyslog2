1a94c8eeca13a74c958f5788a54e750582fb6e3e
root <root@dhcp17-231.ics.muni.cz>
2014-07-14 11:22:46 +0200

    oprava chybejiciho skriptu

--- puppet/fprobe.install.sh
+++ puppet/fprobe.install.sh
@@ -2,4 +2,6 @@ puppet apply -vd avahi.pp
 
 export FACTER_rediser_server=$(avahi-browse -t _rediser._tcp --resolve -p | grep "=;.*;IPv4;" | awk -F";" '{print $8}' | xargs host -t A | rev | awk '{print $1}' | rev | sed 's/\.$//')
 
+puppet apply fprobe.pp
+
 
