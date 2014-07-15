13ad8c38fa7a58e3b8a5efefcd52201df6da587b
root <root@debian>
2014-07-15 12:31:25 +0200

    prejmenovani testu na automaticke spousteni kontrola stari klienta pri testovani

--- puppet/tests/test_rsyslog-client_netstat.sh
+++ puppet/tests/test_rsyslog-client_netstat.sh
@@ -7,6 +7,12 @@ if [ $? -ne 0 ]; then
 	rreturn 1 "$0 rsyslogd check_procs"
 fi
 
+RCAGE=$(ps h -o etimes $(pgrep -f rsyslogd))
+if [ $RCAGE -lt 60 ] ; then
+	echo "INFO: rsyslog-client warming up"
+	sleep 60
+fi
+
 netstat -nlpa | grep "$(pidof rsyslogd)/rsy" | grep ESTA | grep :51[456]
 if [ $? -ne 0 ]; then
 	rreturn 1 "$0 rsyslogd shipper"
