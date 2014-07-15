cbcbc35762b61ae7ff87d861529e5529599acf32
root <root@dhcp17-231.ics.muni.cz>
2014-07-15 20:29:34 +0200

    oprava nz filteru a templaty pridani dashboardu

---
+++ puppet/elk/tailog.sh
@@ -0,0 +1,1 @@
+tail -f /var/log/elasticsearch/*/* /var/log/logstash/*
--- puppet/rediser/rediser-nz-filter.sh
+++ puppet/rediser/rediser-nz-filter.sh
@@ -3,8 +3,9 @@ BASE=/puppet/rediser
 cd /tmp || exit 1
 
 export A=`${BASE}/getpgrp`
-LOGGER="logger -p syslog.info -t rediser-nz-filter.sh[$$]"
+NAME="nz"
+LOGGER="logger -p syslog.info -t rediser-$NAME-filter.sh[$$]"
 
 (stdbuf -i0 -o0 -e0 tr -c '[:print:][:cntrl:]' '?'; $LOGGER "fail tr"; sleep 20; pkill -g $A) | \
-(ruby $BASE/rediser4.rb netflow 1000 |$LOGGER; $LOGGER "fail rediser-nz"; sleep 20; pkill -g $A)
+(ruby $BASE/rediser4.rb $NAME 1000 |$LOGGER; $LOGGER "fail rediser-$NAME"; sleep 20; pkill -g $A)
 
