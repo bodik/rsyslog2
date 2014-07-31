776f08c23492a534cfa6494e3f3bc7ab2f2c2bbc
root <root@debian>
2014-07-31 18:05:11 +0200

    pridani zavislosti

--- puppet/rsyslog-dev.pp
+++ puppet/rsyslog-dev.pp
@@ -20,8 +20,9 @@ package {
 	[ "debhelper", "dh-autoreconf", "dh-systemd", "zlib1g-dev", "libmysqlclient-dev", "libpq-dev", "libmongo-client-dev", 
 	"libcurl4-gnutls-dev", "libkrb5-dev", "libgnutls-dev", "librelp-dev", "libestr-dev", "libee-dev",
  	"liblognorm-dev", "liblogging-stdlog-dev", "libjson-c-dev", "uuid-dev", "pkg-config", "bison",
-	"libgcrypt-dev", "flex"]:
+	"libgcrypt-dev", "flex", "libgnutls28-dev"]:
 	ensure => installed,
+	require => [File["/etc/apt/sources.list.d/sid.list"],Exec["apt-get update"]],
 }
 
 
