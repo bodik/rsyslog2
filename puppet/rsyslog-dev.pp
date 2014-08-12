1bb304b086cf30ec3f60412a3341b0c2f8dae3ab
root <root@debian>
2014-08-12 11:13:10 +0200

    rsyslog 8.2 je uz v jessie

--- puppet/rsyslog-dev.pp
+++ puppet/rsyslog-dev.pp
@@ -9,20 +9,20 @@ file { "/etc/apt/sources.list.d/wheezy-backports.list":
         owner => "root", group => "root", mode => "0644",
         notify => Exec["apt-get update"],
 }
-file { "/etc/apt/sources.list.d/sid.list":
-        source => "/puppet/templates/etc/apt/sources.list.d/sid.list",
+file { "/etc/apt/sources.list.d/jessie.list":
+        source => "/puppet/templates/etc/apt/sources.list.d/jessie.list",
         owner => "root", group => "root", mode => "0644",
         notify => Exec["apt-get update"],
 }
 
-# v7.6 build deps
+# v8.2 build deps
 package { 
 	[ "debhelper", "dh-autoreconf", "dh-systemd", "zlib1g-dev", "libmysqlclient-dev", "libpq-dev", "libmongo-client-dev", 
 	"libcurl4-gnutls-dev", "libkrb5-dev", "libgnutls-dev", "librelp-dev", "libestr-dev", "libee-dev",
  	"liblognorm-dev", "liblogging-stdlog-dev", "libjson-c-dev", "uuid-dev", "pkg-config", "bison",
 	"libgcrypt-dev", "flex", "libgnutls28-dev"]:
 	ensure => installed,
-	require => [File["/etc/apt/sources.list.d/sid.list"],Exec["apt-get update"]],
+	require => [File["/etc/apt/sources.list.d/jessie.list"],Exec["apt-get update"]],
 }
 
 
--- puppet/rsyslog.pp
+++ puppet/rsyslog.pp
@@ -10,14 +10,14 @@ class rsyslog {
 
 
 	file { "/etc/apt/sources.list.d/sid.list":
-	        source => "/puppet/templates/etc/apt/sources.list.d/sid.list",
+	        source => "/puppet/templates/etc/apt/sources.list.d/jessie.list",
 	        owner => "root", group => "root", mode => "0644",
 	        notify => Exec["apt-get update"],
 	}
-	exec { "install_rsyslog_wheezy-backports":
-		command => "/usr/bin/apt-get update;/usr/bin/apt-get install -q -y -o DPkg::Options::=--force-confold  -t sid rsyslog rsyslog-gssapi rsyslog-relp",
+	exec { "install_rsyslog_jessie":
+		command => "/usr/bin/apt-get update;/usr/bin/apt-get install -q -y -o DPkg::Options::=--force-confold  -t jessie rsyslog rsyslog-gssapi rsyslog-relp",
 		timeout => 600,
 		unless => "/usr/bin/dpkg -l rsyslog | grep ' 8\\.[0-9]'",
-		require => [File["/etc/apt/sources.list.d/sid.list"], Package["rsyslog", "rsyslog-gssapi", "rsyslog-relp"]],
+		require => [File["/etc/apt/sources.list.d/jessie.list"], Package["rsyslog", "rsyslog-gssapi", "rsyslog-relp"]],
 	}
-}
+
