7fb709bc5b3c6fe47317fbc891a082327a14cd6c
root <root@debian>
2014-08-15 16:01:15 +0200

    prechod na vlastni balicek

--- puppet/rsyslog.pp
+++ puppet/rsyslog.pp
@@ -19,11 +19,12 @@ class rsyslog {
 	        owner => "root", group => "root", mode => "0644",
 	        notify => Exec["apt-get update"],
 	}
-	exec { "install_rsyslog_jessie":
-		command => "/usr/bin/apt-get update;/usr/bin/apt-get install -q -y -o DPkg::Options::=--force-confold  -t jessie rsyslog rsyslog-gssapi rsyslog-relp",
+	exec { "install_rsyslog":
+		#command => "/usr/bin/apt-get update;/usr/bin/apt-get install -q -y -o DPkg::Options::=--force-confold  -t jessie rsyslog rsyslog-gssapi rsyslog-relp",
+		command => "/usr/bin/apt-get update;/usr/bin/apt-get install -q -y -o DPkg::Options::=--force-confnew rsyslog=8.2.2-3.rb20 rsyslog-gssapi=8.2.2-3.rb20 rsyslog-relp=8.2.2-3.rb20",
 		timeout => 600,
 		unless => "/usr/bin/dpkg -l rsyslog | grep ' 8\\.[0-9]'",
-		require => [File["/etc/apt/sources.list.d/jessie.list"], Package["rsyslog", "rsyslog-gssapi", "rsyslog-relp"]],
+		require => [File["/etc/apt/sources.list.d/jessie.list", "/etc/apt/sources.list.d/meta-rsyslog.list"], Package["rsyslog", "rsyslog-gssapi", "rsyslog-relp"]],
 
 	}
 }
