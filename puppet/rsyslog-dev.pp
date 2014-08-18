9e469b9831114a496356d5a94ba7924f83cf600d
root <root@dhcp17-231.ics.muni.cz>
2014-08-18 17:17:32 +0200

    rozhozeni rsyslog-dev8 a 7

--- puppet/rsyslog-dev.pp
+++ puppet/rsyslog-dev.pp
@@ -6,25 +6,21 @@ file { "/etc/apt/sources.list.d/wheezy-backports.list":
         owner => "root", group => "root", mode => "0644",
         notify => Exec["apt-get update"],
 }
-file { "/etc/apt/sources.list.d/jessie.list":
-        source => "/puppet/templates/etc/apt/sources.list.d/jessie.list",
-        owner => "root", group => "root", mode => "0644",
-        notify => Exec["apt-get update"],
-}
 
 package { ["dpkg-dev", "gcc", "make", "fakeroot", "git-buildpackage", "debhelper", "dh-autoreconf", "dh-systemd", "bison", "pkg-config"]:
 	ensure => installed,
-	require => [File["/etc/apt/sources.list.d/jessie.list"],Exec["apt-get update"]],
+	require => [File["/etc/apt/sources.list.d/wheezy-backports.list"],Exec["apt-get update"]],
 }
 
-# v8.2 build deps
+# v7.6 build deps
 package { 
-	[ "zlib1g-dev", "libmysqlclient-dev", "libpq-dev", "libmongo-client-dev", "libcurl4-gnutls-dev", 
-	  "libkrb5-dev", "libgnutls-dev", "librelp-dev", "libestr-dev", "libee-dev", "liblognorm-dev", 
-	  "liblogging-stdlog-dev", "libjson-c-dev", "uuid-dev", "libgcrypt-dev", "flex", "libgnutls28-dev"
+	[ 
+	"zlib1g-dev", "libmysqlclient-dev", "libpq-dev", "libmongo-client-dev", "libcurl4-gnutls-dev", "libkrb5-dev", 
+	"libgnutls28-dev", "librelp-dev", "libestr-dev", "libee-dev", "liblognorm-dev", 
+	"liblogging-stdlog-dev", "libjson-c-dev", "uuid-dev"
 	]:
 	ensure => installed,
-	require => [File["/etc/apt/sources.list.d/jessie.list"],Exec["apt-get update"]],
+	require => [File["/etc/apt/sources.list.d/wheezy-backports.list"],Exec["apt-get update"]],
 }
 
 
