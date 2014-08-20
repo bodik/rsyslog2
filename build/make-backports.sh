823c5076960dc5beb62f78e114a8a1ce20687c4e
root <root@dhcp17-231.ics.muni.cz>
2014-08-20 23:41:51 +0200

    finalizace make-backports.sh pro rsyslog 7.6

--- build/make-backports.sh
+++ build/make-backports.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
 
+backport() {
+	#src balik se jmenuje jinak nez vysledek, peklo
+	PKG=$1
+	DIR=$2
+	
+	find . -name "${PKG}*deb" | grep "${PKG}*deb"
+	if [ $? = 1 ]; then
+		apt-get source ${PKG}
+		cd $(find . -maxdepth 1 -type d -name "${DIR}*") || exit 1
+		sed -i '1 s/)/.rb20)/' debian/changelog
+		dpkg-buildpackage -rfakeroot
+		cd ..
+		ls -l 
+	fi
+}
+
 cd /tmp || exit 1
 
 mkdir build-area
 cd build-area || exit 1
 
-
-find . -name "libestr*deb" | grep "libestr*deb"
-if [ $? = 1 ]; then
-	apt-get source libestr
-	cd $(find . -maxdepth 1 -type d -name "libestr*") || exit 1a
-	sed -i '1 s/)/.rb20)/' debian/changelog
-	dpkg-buildpackage -rfakeroot
-	cd ..
-	ls -l 
-fi
-
-find . -name "librelp*deb" | grep "librelp*deb"
-if [ $? = 1 ]; then
-	apt-get source librelp
-	cd $(find . -maxdepth 1 -type d -name "librelp*") || exit 1
-	sed -i '1 s/)/.rb20)/' debian/changelog
-	dpkg-buildpackage -rfakeroot
-	cd ..
-	ls -l
-fi
+backport libestr libestr
+backport librelp librelp
+backport liblogging-stdlog0 liblogging
+backport libjson-c2 json-c
+backport init-system-helpers init-system-helpers
 
