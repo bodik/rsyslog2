b3da850c0e8cd2ba8448b2b6984d48215b6de9b4
root <root@minos47-2.zcu.cz>
2014-08-21 20:02:01 +0200

    vraceni poznamek pro force instal verze balicku

--- puppet/rsyslog.pp
+++ puppet/rsyslog.pp
@@ -10,6 +10,15 @@ class rsyslog {
 	#        owner => "root", group => "root", mode => "0644",
 	#        notify => Exec["apt-get update"],
 	#}
+	#exec { "install_rsyslog":
+        #       #command => "/usr/bin/apt-get update;/usr/bin/apt-get install -q -y -o DPkg::Options::=--force-confold  -t jessie rsyslog rsyslog-gssapi rsyslog-relp",
+        #       #command => "/usr/bin/apt-get update;/usr/bin/apt-get install -q -y -o DPkg::Options::=--force-confnew --force-yes rsyslog=8.2.2-3.rb20 rsyslog-gssapi=8.2.
+        #       command => "/usr/bin/apt-get update;/usr/bin/apt-get install -q -y -o DPkg::Options::=--force-confnew --force-yes rsyslog=7.6.3-3.rb20 rsyslog-gssapi=7.6.3
+        #       timeout => 600,
+        #       unless => "/usr/bin/dpkg -l rsyslog | grep ' 8\\.[0-9]'",
+        #       require => [File["/etc/apt/sources.list.d/wheezy-backports.list", "/etc/apt/sources.list.d/meta-rsyslog.list"], Package["rsyslog", "rsyslog-gssapi", "rsysl
+	#}
+
 
 
 	file { "/etc/apt/sources.list.d/meta-rsyslog.list":
