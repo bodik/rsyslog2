d786415fbf41bb388d2be5046459e62e2cae81e6
root <root@debian>
2014-08-29 10:27:43 +0200

    komentare k zakladani a praci s repozitarem

--- build/make-deb.sh
+++ build/make-deb.sh
@@ -3,7 +3,14 @@
 cd /tmp || exit 1
 
 if [ ! -d collab-maint-rsyslog ]; then
+
+	# repozitar vytvoreny clonem z collabu
+	# git://anonscm.debian.org/collab-maint/rsyslog.git
+	# musi byt navic nastaveny jako 'git config remote.origin.fetch 'refs/heads/*:refs/heads/*''
+	# aby se do nej daly natahovat nove veci z upstreamu upstreamu 'git fetch --all'
+	# po fetchi se navic musi rucne dat 'git update-server-info' protoze na fetch neni hook
 	git clone http://home.zcu.cz/~bodik/meta/git/collab-maint-rsyslog
+
 	cd collab-maint-rsyslog
 	git remote set-url origin --push bodik@bodik.zcu.cz:/afs/zcu.cz/users/b/bodik/public/meta/git/collab-maint-rsyslog
 else
