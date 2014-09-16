87f32032df001e7f540a5ca16662cdbba0287297
root <root@debian>
2014-09-16 17:17:25 +0200

    vyvoj jenkisfull puppet

--- puppet/jenkins.check.sh
+++ puppet/jenkins.check.sh
@@ -2,7 +2,7 @@ if [ -f /etc/default/jenkins ]; then
         echo "INFO: JENKINSCHECK ======================="
 
         for all in jenkins; do
-                echo "INFO: puppet apply -v --noop --show_diff $all.pp"
-                puppet apply -v --noop --show_diff $all.pp
+                echo "INFO: puppet apply -v --noop --show_diff --modulepath=/puppet -e \"include $all\""
+		puppet apply -v --noop --show_diff --modulepath=/puppet -e "include $all"
         done
 fi
