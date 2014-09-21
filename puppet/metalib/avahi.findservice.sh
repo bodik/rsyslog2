54a75f4b5fe657f9653fb5c3bb468aaedf364d2e
root <root@metacloud.127.0.0.1.xip.io>
2014-09-21 08:43:15 -0700

    fixed avahi discovery without proper dns (eg vagrant testing)

--- puppet/metalib/avahi.findservice.sh
+++ puppet/metalib/avahi.findservice.sh
@@ -12,9 +12,15 @@ fi
 
 #avahi-browse -t $1 --resolve -p | grep "=;.*;IPv4;" | awk -F";" '{print $8}' | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n | tail -1 | xargs host -t A | rev | awk '{print $1}' | rev | sed 's/\.$//'
 IP=$(avahi-browse -t $1 --resolve -p | grep "=;.*;IPv4;" | awk -F";" '{print $8}' | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n | tail -1)
+
 if [ -z $IP ]; then
 	exit 0
 else
-	host -t A $IP | rev | awk '{print $1}' | rev | sed 's/\.$//'	
-	exit 1
+	HNAME=$(host -t A $IP)
+	if [ $? -ne 0 ]; then
+		echo $IP
+	else
+		echo $HNAME | rev | awk '{print $1}' | rev | sed 's/\.$//'
+	fi
+	exit 0
 fi
