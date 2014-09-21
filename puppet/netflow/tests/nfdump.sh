3cd7a520e83c9c3aca13f3006a1e813714397a86
root <root@metacloud.127.0.0.1.xip.io>
2014-09-21 09:19:55 -0700

    refined nfdump pmacct usage test

--- puppet/netflow/tests/nfdump.sh
+++ puppet/netflow/tests/nfdump.sh
@@ -3,7 +3,7 @@
 . /puppet/metalib/lib.sh
 
 #sw emitor
-/usr/lib/nagios/plugins/check_procs -C pmacctd
+/usr/lib/nagios/plugins/check_procs -C pmacctd -c 2:2
 if [ $? -ne 0 ]; then
 	rreturn 1 "$0 pmacctd check_procs"
 fi
