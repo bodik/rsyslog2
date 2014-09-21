7208bcea56abbdfda8bfd3b989dbeee39dedc942
root <root@dhcp17-231.ics.muni.cz>
2014-09-22 01:01:00 +0200

    removed not used parameter tuned documentation

--- puppet/metalib/manifests/avahi.pp
+++ puppet/metalib/manifests/avahi.pp
@@ -1,20 +1,23 @@
 # == Class: avahi
 #
-# Support class for installling avahi utils etc.
+# Support class for installling avahi utils and resolving daemon. This class is used
+# during dynamic cloud autodiscovery by other classes.
 #
 # === Examples
 #
-#  class { avahi:  }
+# include metalib::avahi
 #
 # === Authors
 #
 # bodik@cesnet.cz
 #
-class metalib::avahi (
-	$daemon_ensure = running,
-) {
+class metalib::avahi ( ) {
 	package { ["avahi-daemon", "avahi-utils"]:
 	        ensure => installed,
 	}
-	service { "avahi-daemon": ensure => $daemon_ensure, }
+	service { "avahi-daemon": 
+		ensure => running, 
+		hasstatus => false,
+		status    => "/bin/ps ax | grep -v grep | grep avahi-daemon",
+	}
 }
