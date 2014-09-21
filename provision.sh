c98348bbb3f2558410dd676f4ef95e2527fb68b2
jan kaufman <jan.kaufman@gooddata.com>
2014-09-21 16:23:51 +0200

    CONFIG: fprobe to nfdump

--- provision.sh
+++ provision.sh
@@ -5,7 +5,7 @@ cd /puppet
 sh phase2.install.sh
 sh rediser.install.sh
 sh elk.install.sh
-sh fprobe.install.sh
+sh nfdump.install.sh
 sh elk/tests/elk.sh
 sh netflow/tests/nfdump.sh
 sh rediser/tests/rediser.sh
