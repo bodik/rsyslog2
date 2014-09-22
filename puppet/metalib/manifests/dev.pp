d1c59c02aae3cf623b94d5d04a765b4888582595
bodik <bodik@bodik.zcu.cz>
2014-09-23 00:09:26 +0200

    documentation development

--- puppet/metalib/manifests/dev.pp
+++ puppet/metalib/manifests/dev.pp
@@ -1,7 +1,7 @@
 # == Class: metalib::dev
 #
-# Class for installling set of development and debugging packages. Used
-# manually when needed
+# Class for installling set of development, debugging and packages needed
+# for documentation generation. Used manually when needed.
 #
 # === Examples
 #
@@ -10,6 +10,18 @@
 class metalib::dev { 
 	package { ["tcpdump", "strace", "puppet-lint", "colordiff", "augeas-tools", "ngrep", "iotop", "atop", "rake"]: 
 		ensure => installed, 
+	}
+
+	package { "ruby-dev":
+		ensure => installed,
+	}
+	package { ['redcarpet' ,'rdoc', 'github-markup']:
+		ensure   => 'installed',
+		provider => 'gem',
+		require => Package["ruby-dev"],
+	}
+	package { "links":
+		ensure => installed,
 	}
 
 	#package { ["openjdk-7-jdk"]:
