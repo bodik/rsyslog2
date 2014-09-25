d364ee52d924acdf5b11e2d749ae002b123ce501
root <root@dhcp17-231.ics.muni.cz>
2014-09-25 10:12:23 +0200

    fixing check for detection

--- puppet/glastopf.check.sh
+++ puppet/glastopf.check.sh
@@ -1,6 +1,6 @@
-dpkg -l fprobe 1>/dev/null 2>/dev/null
+test -d /opt/glastopf 1>/dev/null 2>/dev/null
 if [ $? -eq 0 ]; then
-        echo "INFO: FPROBECHECK ======================="
+        echo "INFO: GLASTOPFCHECK ======================="
 
         echo "INFO: puppet apply --modulepath=/puppet -v --noop --show_diff -e 'include glastopf'"
         puppet apply --modulepath=/puppet -v --noop --show_diff -e 'include glastopf'
