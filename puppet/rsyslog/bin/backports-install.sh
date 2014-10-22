1445514ce1f28ed94ec2cd1f5cb5f6ad8ba99246
root <root@debian>
2014-10-22 10:59:39 +0200

    dev packages are must for rsyslog build

--- puppet/rsyslog/bin/backports-install.sh
+++ puppet/rsyslog/bin/backports-install.sh
@@ -1,2 +1,2 @@
 dpkg --purge libjson0:i386 libpulse0:i386 ia32-libs-i386 libsdl1.2debian:i386 ia32-libs
-find /tmp/build-area/ -name "*deb" ! -name "*-dev*" ! -name "*-doc*" -exec dpkg -i {} \;
+find /tmp/build-area/ -name "*deb" ! -name "*-doc*" -exec dpkg -i {} \;
