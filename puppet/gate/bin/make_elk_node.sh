f10a5ba9111540bf8ebe44ab1fc528083c0cf7b9
root <root@stoor1.zcu.cz>
2015-03-25 01:02:51 +0100

    finalization of simple deploy script

--- puppet/gate/bin/make_elk_node.sh
+++ puppet/gate/bin/make_elk_node.sh
@@ -1,4 +1,1 @@
-wget http://esb3.metacentrum.cz/rsyslog2.git/bootstrap.install.sh && sh bootstrap.install.sh
-cd /puppet
-sh phase2.install.sh
-sh elk.install.sh
+ssh root@$1 'wget http://esb3.metacentrum.cz/rsyslog2.git/bootstrap.install.sh && sh bootstrap.install.sh; cd /puppet; sh phase2.install.sh; sh elk.install.sh'
