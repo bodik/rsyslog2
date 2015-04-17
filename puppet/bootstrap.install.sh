b60eaaa601cb6f7a863bb61dac598c9307254caf
Radoslav Bodo <bodik@cesnet.cz>
2015-04-17 19:18:53 +0200

    wardenb bootstrap changes

--- puppet/bootstrap.install.sh
+++ puppet/bootstrap.install.sh
@@ -2,14 +2,14 @@ apt-get update
 apt-get install -y git puppet
 
 if [ ! -d /puppet ]; then
-	cd /
-	git clone http://esb3.metacentrum.cz/rsyslog2.git
-	ln -sf /rsyslog2/puppet /puppet
+        cd /
+        git clone http://esb3.metacentrum.cz/wardenb.git
+        ln -sf /wardenb /puppet
 else
-	cd /puppet
-	git remote set-url origin http://esb3.metacentrum.cz/rsyslog2.git
-	git pull
+        cd /puppet
+        git remote set-url origin http://esb3.metacentrum.cz/wardenb.git
+        git pull
 fi
 
-cd /puppet && git remote set-url origin bodik@esb3.metacentrum.cz:/data/rsyslog2.git
+cd /puppet && git remote set-url origin bodik@esb3.metacentrum.cz:/data/wardenb.git
 
