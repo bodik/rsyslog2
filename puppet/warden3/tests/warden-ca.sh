951d1d70d4e5752e5bad8331691a9c3119c4691c
root <root@debian>
2015-04-20 23:50:28 +0200

    import selftest deleted vanila test taken from warden3

---
+++ puppet/warden3/tests/warden-ca.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# modified by bodik@cesnet.cz
+
+. /puppet/metalib/lib.sh
+
+WS=$(/puppet/metalib/avahi.findservice.sh _warden-server._tcp)
+if [ -z "$WS" ]; then
+        echo "ERROR: cannt discover warden_ca server"
+        exit 1
+fi
+
+curl -k "http://${WS}:45444/getCaCertificate" 1>/dev/null 2>/dev/null
+if [ $? -ne 0 ]; then
+	rreturn 1 "$0 ca not running"
+fi
+
+rreturn 0 "$0"
