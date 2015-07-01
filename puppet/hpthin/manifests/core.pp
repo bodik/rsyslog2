c99d7b082f430ed54ce91b0625e98adec8ed8a8a
root <root@took57.ics.muni.cz>
2015-07-01 21:54:33 +0200

    forwarding and conditionals

--- puppet/hpthin/manifests/core.pp
+++ puppet/hpthin/manifests/core.pp
@@ -16,13 +16,14 @@ class hpthin::core (
 		command => "/sbin/ip tunnel add ${core_tun_dev} mode gre local ${ipaddress} remote ${thin_public_address} key ${key}",
 	}
 	exec { "tunnel link":
-		command => "/sbin/ip set dev ${core_tun_dev} up",
+		command => "/sbin/ip link set dev ${core_tun_dev} up",
 	}
 	exec { "tunnel address":
-		command => "/sbin/ip address ${core_tun_address}/30",
+		command => "/sbin/ip address add ${core_tun_address}/30 dev ${core_tun_dev}",
+		unless => "/sbin/ip addr | /bin/grep ${core_tun_address}",
 	}
 	exec { "route table":
-		command => "/bin/echo '${table_num} ${table_name}' >> /etc/iproute2/tables",
+		command => "/bin/echo '${table_num} ${table_name}' >> /etc/iproute2/rt_tables",
 	}
 	exec { "route add":
 		command => "/sbin/ip route add default table ${table_name} via ${thin_tun_address}",
@@ -33,5 +34,8 @@ class hpthin::core (
 	exec { "iptables backroute":
 		command => "/sbin/iptables -t mangle -A OUTPUT -s ${core_tun_address} -j MARK --set-mark ${table_num}",
 	}
-	Exec["tunnel"]->Exec["tunnel link"]->Exec["tunnel address"]->Exec["route table"]->Exec["route add"]->Exec["route rule"]->Exec["iptables backroute"]
+	exec { "ip forwarding":
+		command => "/bin/echo 1 > /proc/sys/net/ipv4/ip_forward",
+	}
+	Exec["tunnel"]->Exec["tunnel link"]->Exec["tunnel address"]->Exec["route table"]->Exec["route add"]->Exec["route rule"]->Exec["iptables backroute"]->Exec["ip forwarding"]
 }
