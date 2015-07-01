bff2e41db59092feea198b4e0d96dd7d81c344b9
root <root@vmes2.metacentrum.cz>
2015-07-01 17:16:34 +0000

    right setting pinning 1.4.2 (thanks whack)

--- puppet/mongomine/manifests/lsl.pp
+++ puppet/mongomine/manifests/lsl.pp
@@ -46,6 +46,7 @@ class mongomine::lsl (
 	class { 'logstash':
 		manage_repo  => true,
 		repo_version => '1.4',
+		version => '1.4.2-*',
 		install_contrib => true,
 	}
 	file { '/etc/logstash/patterns/metacentrum':
