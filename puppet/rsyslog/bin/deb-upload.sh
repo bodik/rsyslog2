b74104b1e8b3ffd07450450f3003f0a8c8baa8bb
root <root@minos48-2.zcu.cz>
2015-07-15 21:02:24 +0200

    dev

--- puppet/rsyslog/bin/deb-upload.sh
+++ puppet/rsyslog/bin/deb-upload.sh
@@ -1,14 +1,14 @@
 
-FRONT='bodik@bodik.zcu.cz'
-REPO="/afs/zcu.cz/users/b/bodik/public/metasw/rsyslog2/packages"
+FRONT='bodik@esb.metacentrum.cz'
+REPO="/opt/packages"
 
 cd /tmp/build-area
 
 dpkg-scanpackages ./ /dev/null | gzip > Packages.gz
 
-ssh $FRONT "find /afs/zcu.cz/users/b/bodik/public/metasw/rsyslog2/packages -type f -delete"
+ssh $FRONT "find /opt/packages -type f -delete"
 scp * ${FRONT}:${REPO}/
-# >>> deb http://home.zcu.cz/~bodik/metasw/rsyslog2/packages ./
+# >>> deb http://esb.metacentrum.cz/packages ./
 
 
 
