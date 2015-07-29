849bf685147129341b46b6cf69aaf52cb3f31923
root <root@minos47-2.zcu.cz>
2015-07-29 14:01:07 +0200

    force 1.6 version (ther's 1.0 in jessie which has prefs by default...)

--- puppet/elk/manifests/esd.pp
+++ puppet/elk/manifests/esd.pp
@@ -37,6 +37,7 @@ class elk::esd (
 	class { 'elasticsearch':
 		manage_repo  => true,
 		repo_version => '1.6',
+		version => "1.6.*",
 		java_install => true,
 		datadir => '/scratch',
 		init_defaults => $config_hash,
