2ba093924aa24b7d37754c70f64b11a8edd62d1a
root <root@took47.ics.muni.cz>
2015-09-30 17:32:36 +0200

    import

--- puppet/hpdio/manifests/init.pp
+++ puppet/hpdio/manifests/init.pp
@@ -97,31 +97,48 @@ class hpdio (
 
 
 
-##	# warden_client
-##	file { "${install_dir}/warden_client.py":
-##		source => "puppet:///modules/${module_name}/warden_client.py",
-##		owner => "$telnetd_user", group => "$telnetd_user", mode => "0755",
-##		require => File["${install_dir}"],
-##	}
-##	$fqdn_rev = myexec("echo ${fqdn} | awk '{n=split(\$0,A,\".\");S=A[n];{for(i=n-1;i>0;i--)S=S\".\"A[i]}}END{print S}'")
-##	file { "${install_dir}/warden_client.cfg":
-##		content => template("${module_name}/warden_client.cfg.erb"),
-##		owner => "$telnetd_user", group => "$telnetd_user", mode => "0640",
-##		require => File["${install_dir}"],
-##	}
-##	$anonymised_target_net = myexec("/usr/bin/facter ipaddress | sed 's/\\.[0-9]*\\.[0-9]*\\.[0-9]*$/.0.0.0/'")
-##	file { "${install_dir}/warden_client-telnetd.cfg":
-##		content => template("${module_name}/warden_client-telnetd.cfg.erb"),
-##		owner => "$telnetd_user", group => "$telnetd_user", mode => "0640",
-##		require => File["${install_dir}"],
-##	}
-##	class { "warden3::hostcert": 
-##		warden_server => $warden_server_real,
-##	}
-##	exec { "register telnetd sensor":
-##		command	=> "/bin/sh /puppet/warden3/bin/register_sensor.sh -s ${warden_server_real} -n telnetd -d ${install_dir}",
-##		creates => "${install_dir}/registered-at-warden-server",
-##		require => File["${install_dir}"],
-##	}
+
+
+	# warden_client pro kippo (basic w3 client, reporter stuff, run/persistence/daemon)
+	file { "${install_dir}/warden":
+		ensure => directory,
+		owner => "${dio_user}", group => "${dio_user}", mode => "0755",
+	}
+	file { "${install_dir}/warden/warden_client.py":
+		source => "puppet:///modules/${module_name}/warden_client/warden_client.py",
+		owner => "${dio_user}", group => "${dio_user}", mode => "0755",
+		require => File["${install_dir}/warden"],
+	}
+	$fqdn_rev = myexec("echo ${fqdn} | awk '{n=split(\$0,A,\".\");S=A[n];{for(i=n-1;i>0;i--)S=S\".\"A[i]}}END{print S}'")
+	file { "${install_dir}/warden/warden_client.cfg":
+		content => template("${module_name}/warden_client.cfg.erb"),
+		owner => "${dio_user}", group => "${dio_user}", mode => "0640",
+		require => File["${install_dir}/warden"],
+	}
+	class { "warden3::hostcert": 
+		warden_server => $warden_server_real,
+	}
+	exec { "register dio sensor":
+		command	=> "/bin/sh /puppet/warden3/bin/register_sensor.sh -s ${warden_server_real} -n dio -d ${install_dir}",
+		creates => "${install_dir}/registered-at-warden-server",
+		require => Exec["do install"],
+	}
+
+	file { "${install_dir}/warden/warden3-dio-sender.py":
+		source => "puppet:///modules/${module_name}/hp-dio/warden3-dio-sender.py",
+		owner => "${dio_user}", group => "${dio_user}", mode => "0755",
+		require => File["${install_dir}/warden"],
+	}
+	$anonymised_target_net = myexec("/usr/bin/facter ipaddress | sed 's/\\.[0-9]*\\.[0-9]*\\.[0-9]*$/.0.0.0/'")
+	file { "${install_dir}/warden/warden_client-dio.cfg":
+		content => template("${module_name}/warden_client-dio.cfg.erb"),
+		owner => "${dio_user}", group => "${dio_user}", mode => "0640",
+		require => File["${install_dir}/warden"],
+	}
+	file { "/etc/cron.d/warden-dio":
+		content => template("${module_name}/warden-dio.cron.erb"),
+		owner => "root", group => "root", mode => "0644",
+		require => User["$dio_user"],
+	}
 
 }
