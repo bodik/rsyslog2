fbf1763bed9d702bdc39a2474250ad5592a1c6cc
root <root@took55.ics.muni.cz>
2015-10-07 16:45:24 +0200

    sip to dio

--- puppet/hpdio/tests/hpdio.sh
+++ puppet/hpdio/tests/hpdio.sh
@@ -15,6 +15,12 @@ if [ $? -ne 0 ]; then
 	rreturn 1 "$0 dionaea 69 listener"
 fi
 
+netstat -nlpa | grep "/dionaea" | grep "^udp.*:5060 "
+if [ $? -ne 0 ]; then
+	rreturn 1 "$0 dionaea 5060 listener"
+fi
+
+
 
 ls -l /opt/dionaea/var/dionaea/logsql.sqlite
 if [ $? -ne 0 ]; then
