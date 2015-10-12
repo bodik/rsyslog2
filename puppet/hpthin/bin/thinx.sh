298ed82f7735b5676ab42805d8c192efc95e1735
root <root@took54.ics.muni.cz>
2015-10-12 15:23:25 +0200

    thinx dev

--- puppet/hpthin/bin/thinx.sh
+++ puppet/hpthin/bin/thinx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-usage() { echo "Usage: $0 -d <THIN_TUN_DEV> -t <THIN_TUN_ADDRESS> -n <CORE_TUN_NUMBER> -c <CORE_PUBLIC_ADDRESS> -p <PORT>,<PORT>,..." 1>&2; exit 1; }
-while getopts "d:t:n:c:" o; do
+usage() { echo "Usage: $0 -d <THIN_TUN_DEV> -t <THIN_TUN_ADDRESS> -n <CORE_TUN_NUMBER> -c <CORE_PUBLIC_ADDRESS> -a <CORE_TUN_ADDRESS> -p <PORT>,<PORT>,..." 1>&2; exit 1; }
+while getopts "d:t:n:c:a:p:" o; do
     case "${o}" in
         d)
             thin_tun_dev=${OPTARG}
@@ -15,8 +15,11 @@ while getopts "d:t:n:c:" o; do
         c)
             core_public_address=${OPTARG}
             ;;
+        a)
+            core_tun_address=${OPTARG}
+            ;;
         p)
-            ports=${OPTARG}
+            port_forwards=${OPTARG}
             ;;
         *)
             usage
@@ -25,7 +28,7 @@ while getopts "d:t:n:c:" o; do
 done
 shift $((OPTIND-1))
 
-hashkey = "$core_tun_number--$core_public_address"
+hashkey="$core_tun_number--$core_public_address"
 cd /tmp || exit 1
 
 /sbin/ip tunnel show | /bin/grep ${thin_tun_dev}
@@ -41,19 +44,19 @@ iptables-restore < /tmp/w3thin.iptables.cleaned
 rm -f /tmp/w3thin.iptables.current /tmp/w3thin.iptables.cleaned
 
 echo "INFO: $hashkey: creating tunnel"
-/sbin/ip tunnel add ${thin_tun_dev} mode gre local ${ipaddress} remote ${core_public_address}
+/sbin/ip tunnel add ${thin_tun_dev} mode gre local $(facter ipaddress) remote ${core_public_address}
 
 echo "INFO: $hashkey: creating tunnel link"
 /sbin/ip link set dev ${thin_tun_dev} up
 
 echo "INFO: $hashkey: tunnel address"
 /sbin/ip addr | /bin/grep ${thin_tun_address}
-if [ $? -eq 0 ];then
+if [ $? -eq 1 ];then
 	/sbin/ip address add ${thin_tun_address}/30 dev ${thin_tun_dev}
 fi
 
 echo "INFO: $hashkey: iptables redirects"
-doports = $(echo ${ports} | sed 's/,/ /')
+doports=$(echo ${port_forwards} | sed 's/,/ /g')
 for all in $doports; do
 	/sbin/iptables -t nat -A PREROUTING -p tcp --dport ${all} -j DNAT --to-destination ${core_tun_address}:${all} -m comment --comment "w3thin"
 done
--- puppet/hpthin/manifests/thinx.pp
+++ puppet/hpthin/manifests/thinx.pp
@@ -8,13 +8,13 @@ define hpthin::thinx ($core_tun_number, $core_public_address, $port_forwards) {
         $thin_tun_address_octet = ($core_tun_number*4)+2
 	$core_tun_address = "10.0.0.${core_tun_address_octet}"
 	$thin_tun_address = "10.0.0.${thin_tun_address_octet}"
-	notice($core_tun_address)
-	notice($thin_tun_address)
-	$hashkey = "$core_tun_number -- $core_public_address"
-	notice($hashkey)
+
+	$hashkey = "$core_tun_number--$core_public_address"
+	$cmd = "/bin/sh -x /puppet/hpthin/bin/thinx.sh -d ${thin_tun_dev} -t ${thin_tun_address} -n ${core_tun_number} -c ${core_public_address} -a ${core_tun_address} -p ${port_forwards}"
+	notice($hashkey, $cmd)
 
 	exec { "$hashkey creating thinx tunnel":
-		command => "/bin/sh /puppet/hpthin/bin/thinx.sh -d ${thin_tun_dev} -t ${thin_tun_address} -n ${core_tun_number} -c ${core_public_address} -p ${ports}"
+		command => "$cmd"
 	}
 
 }
