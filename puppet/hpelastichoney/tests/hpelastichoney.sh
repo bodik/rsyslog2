29121d5f0a470100d6906e4fcb896522d93ca1f4
root <root@took56.ics.muni.cz>
2015-10-20 14:00:05 +0200

    age test in test

--- puppet/hpelastichoney/tests/hpelastichoney.sh
+++ puppet/hpelastichoney/tests/hpelastichoney.sh
@@ -7,6 +7,12 @@ if [ $? -ne 0 ]; then
 	rreturn 1 "$0 elastichoney check_procs"
 fi
 
+AGE=$(ps h -o etimes $(pgrep -f /opt/elastichoney/elastichoney))
+if [ $AGE -lt 10 ] ; then
+	echo "INFO: elastichoney warming up"
+	sleep 10
+fi
+
 NOW=$(date +%s)
 wget -O /dev/null -q "http://$(facter ipaddress):9200/_search?autotest_message_$NOW"
 if [ $? -ne 0 ]; then
