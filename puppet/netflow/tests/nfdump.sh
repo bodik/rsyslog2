6f950ca466adb4b6c7267bff8caa4cd605f17a26
root <root@minos49-2.zcu.cz>
2015-10-22 16:48:37 +0200

    test tuning, nfdump behaves differently in recent versions

--- puppet/netflow/tests/nfdump.sh
+++ puppet/netflow/tests/nfdump.sh
@@ -14,6 +14,15 @@ if [ $? -ne 0 ]; then
 	rreturn 1 "$0 nfcapd check_procs"
 fi
 
-if [ ! -f /var/cache/nfdump/nfcapd.current ]; then
+if [ ! -f /var/cache/nfdump/nfcapd.current.* ]; then
 	rreturn 1 "$0 nfcapd output missing"
 fi
+
+MTIME=$(stat --format=%Y /var/cache/nfdump/nfcapd.current.*)
+NOW=$(date +%s)
+AGE=$(( $NOW - $MTIME ))
+if [ $AGE -gt 70 ]; then
+	rreturn 1 "$0 nfcapd output too old"
+fi
+
+rreturn 0 "$0 ok"
