ca513fa8601386b2bedefb7e9aa32f4b5557d5f8
root <root@took81.ics.muni.cz>
2015-10-24 12:49:18 +0200

    puppetlabs-apt install before update messing with instaltions Revert "monkeypatch elasticsearch puppet module puppetlabs-apt usage"
    This reverts commit b643625db98de5d3deb737b95eb4eff4f3d6f584.

--- puppet/3rdparty/elasticsearch/manifests/repo.pp
+++ puppet/3rdparty/elasticsearch/manifests/repo.pp
@@ -40,9 +40,10 @@ class elasticsearch::repo {
         location    => "http://packages.elastic.co/elasticsearch/${elasticsearch::repo_version}/debian",
         release     => 'stable',
         repos       => 'main',
-  	key         => { 'source' => 'http://packages.elastic.co/GPG-KEY-elasticsearch', 'id' => '46095ACC8548582C1A2699A9D27D666CD88E42B4' },
-	include     => { 'src' => false },
-    }
+        key         => 'D88E42B4',
+        key_source  => 'http://packages.elastic.co/GPG-KEY-elasticsearch',
+        include_src => false,
+      }
     }
     'RedHat', 'Linux': {
       yumrepo { 'elasticsearch':
