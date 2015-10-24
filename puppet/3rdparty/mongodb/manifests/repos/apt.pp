feeb31098e0696b29b42e79895225eb6cfda96f3
root <root@took81.ics.muni.cz>
2015-10-24 13:31:32 +0200

    apt::source in mongodb match 1.8 policy

--- puppet/3rdparty/mongodb/manifests/repos/apt.pp
+++ puppet/3rdparty/mongodb/manifests/repos/apt.pp
@@ -5,11 +5,14 @@ class mongodb::repos::apt {
     default  => 'http://downloads-distro.mongodb.org/repo/debian-sysvinit',
   }
 
+  if !defined(Class['::apt']) {
+        class { '::apt': }
+  }
   apt::source{ '10gen':
     location    => $source_location,
     release     => 'dist',
     repos       => '10gen',
-    key         => '7F0CEB10',
+    key         => '492EAFE8CD016A07919F1D2B9ECBEC467F0CEB10',
     key_server  => 'keyserver.ubuntu.com',
     include_src => false,
   }
