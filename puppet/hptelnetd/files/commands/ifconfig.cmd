07dd3ae498a742d195adcc82d811f5b625990244
root <root@took2.ics.muni.cz>
2015-10-29 23:10:13 +0100

    imported new version of telnetd, manifest not complete dev commit

---
+++ puppet/hptelnetd/files/commands/ifconfig.cmd
@@ -0,0 +1,24 @@
+eth0      Link encap:Ethernet  HWaddr 02:00:93:fb:fd:f2  
+          inet addr: 172.16.33.242  Bcast:172.16.33.255  Mask:255.255.255.128
+          UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+          RX packets:341745 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:50164 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:1000 
+          RX bytes:41576343 (39.6 MiB)  TX bytes:18362664 (17.5 MiB)
+
+eth1      Link encap:Ethernet  HWaddr 02:00:0c:22:01:48  
+          inet addr:192.18.10.92  Bcast:192.18.10.255  Mask:255.255.255.0
+          UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+          RX packets:18053415 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:7 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:1000 
+          RX bytes:1334552483 (1.2 GiB)  TX bytes:1082 (1.0 KiB)
+
+lo        Link encap:Local Loopback  
+          inet addr:127.0.0.1  Mask:255.0.0.0
+          UP LOOPBACK RUNNING  MTU:16436  Metric:1
+          RX packets:0 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:0 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:0 
+          RX bytes:0 (0.0 B)  TX bytes:0 (0.0 B)
+
--- puppet/hptelnetd/tests/hptelnetd.sh
+++ puppet/hptelnetd/tests/hptelnetd.sh
@@ -1,16 +1,10 @@
-#!/bin/sh 
+#!/bin/sh
 
-. /puppet/metalib/lib.sh
+. /puppet/metalib/bin/lib.sh
 
-INSTALL_DIR=/opt/telnetd
-NOW=$(date +%s)
-
-PORT=$(cat $INSTALL_DIR/warden_client-telnetd.cfg | grep twisted_port_spec | awk '{print $2}' | sed 's/"tcp:\([0-9]\+\)"/\1/')
-
-(echo "autotest $NOW"; sleep 1) | telnet $(facter ipaddress) $PORT | grep "login: "; echo $?
+python -c 'print "root\ntoor\nid\nexit"' | timeout 2s nc $(facter ipaddress) 23 | grep 'uid=0'
 if [ $? -ne 0 ]; then
-	rreturn 0 "$0 failed to open telnetd connection"
-fi
+        rreturn 1 "$0 failed to login to hptelnetd"
+fi 
 
 rreturn 0 "$0"
-
