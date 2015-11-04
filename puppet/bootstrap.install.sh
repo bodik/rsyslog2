6fc0e019696753b0181a8982bffa1362f78d4cd5
root <root@debian.domain.cz>
2015-11-04 09:54:57 +0100

    move all activity to esb service name instead of esb3 instance name

--- puppet/bootstrap.install.sh
+++ puppet/bootstrap.install.sh
@@ -3,13 +3,13 @@ apt-get install -y git puppet
 
 if [ ! -d /puppet ]; then
 	cd /
-	git clone http://esb3.metacentrum.cz/rsyslog2.git
+	git clone http://esb.metacentrum.cz/rsyslog2.git
 	ln -sf /rsyslog2/puppet /puppet
 else
 	cd /puppet
-	git remote set-url origin http://esb3.metacentrum.cz/rsyslog2.git
+	git remote set-url origin http://esb.metacentrum.cz/rsyslog2.git
 	git pull
 fi
 
-cd /puppet && git remote set-url origin bodik@esb3.metacentrum.cz:/data/rsyslog2.git
+cd /puppet && git remote set-url origin bodik@esb.metacentrum.cz:/data/rsyslog2.git
 
