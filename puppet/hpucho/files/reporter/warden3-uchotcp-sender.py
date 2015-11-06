bb398803b545ec17f4f6a06baca7c436379dbd78
Michal Kostenec <kostenec@cesnet.cz>
2015-11-06 15:24:22 +0100

    uchotcp splitted to hp and sender

---
+++ puppet/hpucho/files/reporter/warden3-uchotcp-sender.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+from warden_client import Client, Error, read_cfg, format_timestamp
+from time import time, gmtime, strftime
+from math import trunc
+from uuid import uuid4
+import json
+import string
+import os
+import sys
+import w3utils_flab as w3u
+
+aconfig = read_cfg('warden_client-uchotcp.cfg')
+wconfig = read_cfg('warden_client.cfg')
+aclient_name = aconfig['name']
+wconfig['name'] = aclient_name
+aanonymised = aconfig['anonymised']
+aanonymised_net  = aconfig['target_net']
+aanonymised = aanonymised if (aanonymised_net != '0.0.0.0/0') or (aanonymised_net == 'omit') else '0.0.0.0/0'
+wclient = Client(**wconfig)
+
+def gen_event_idea_uchotcp(detect_time, src_ip, src_port, dst_ip, dst_port, proto, decoded, smart, data):
+
+        event = {
+                "Format": "IDEA0",
+                "ID": str(uuid4()),
+                "DetectTime": detect_time,
+                "Category": ["Intrusion"],
+                "Note": "Uchotcp event",
+                "ConnCount": 1,
+                "Source": [{ "Proto": proto, "Port": [src_port] }],
+                "Target": [{ "Proto": proto, "Port": [dst_port] }],
+                "Node": [
+                        {
+                                "Name": aclient_name,
+                                "Tags": ["Honeypot", "Connection"],
+                                "SW": ["Uchotcp"],
+                        }
+                ],
+                "Attach": [{ "data": data, "datalen": len(data) }]
+	}
+
+	if decoded:
+		p = decoded['protocol']
+		event["Attach"][0][p] = {}
+		for key, value in decoded.items():
+			if key != 'protocol':
+				event["Attach"][0][p][key] = value
+	if smart:	
+		event["Attach"][0]["smart"] = smart
+
+        event = w3u.IDEA_fill_addresses(event, src_ip, dst_ip, aanonymised, aanonymised_net)
+
+        return event
+
+events = []
+for line in w3u.Pygtail(filename=aconfig.get('logfile'), wait_timeout=0):
+	data = json.loads(line)
+
+	a = gen_event_idea_uchotcp(
+		detect_time = data['detect_time'], 
+		src_ip      = data['src_ip'],
+		src_port    = data['src_port'], 
+		dst_ip      = data['dst_ip'],
+		dst_port    = data['dst_port'],
+		proto       = data['proto'],
+		decoded     = data['decoded'],
+		smart	    = data['smart'],
+		data        = data['data'],	
+	)
+	#print json.dumps(a)
+	events.append(a)
+
+#print json.dumps(events, indent=3)
+
+print "=== Sending ==="
+start = time()
+ret = wclient.sendEvents(events)
+
+if 'saved' in ret:
+	wclient.logger.info("%d event(s) successfully delivered." % ret['saved'])
+
+print "Time: %f" % (time() - start)
+
