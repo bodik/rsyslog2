5f878fbec61dbdb9ab4278764ac83759ce00fa60
Michal Kostenec <kostenec@cesnet.cz>
2015-11-09 11:41:50 +0100

    uchoweb splitted to hp and sender

---
+++ puppet/hpucho/files/reporter/warden3-uchoweb-sender.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+from warden_client import Client, Error, read_cfg, format_timestamp
+from time import time, gmtime, strftime
+from math import trunc
+from uuid import uuid4
+import json
+import string
+import os
+import sys
+import w3utils_flab as w3u
+
+aconfig = read_cfg('warden_client-uchoweb.cfg')
+wconfig = read_cfg('warden_client.cfg')
+aclient_name = aconfig['name']
+wconfig['name'] = aclient_name
+aanonymised = aconfig['anonymised']
+aanonymised_net  = aconfig['target_net']
+aanonymised = aanonymised if (aanonymised_net != '0.0.0.0/0') or (aanonymised_net == 'omit') else '0.0.0.0/0'
+wclient = Client(**wconfig)
+
+def gen_event_idea_uchoweb(detect_time, src_ip, src_port, dst_ip, dst_port, proto, data):
+
+        event = {
+                "Format": "IDEA0",
+                "ID": str(uuid4()),
+                "DetectTime": detect_time,
+                "Category": ["Other"],
+                "Note": "Uchoweb event",
+                "ConnCount": 1,
+                "Source": [{ "Proto": proto, "Port": [src_port] }],
+                "Target": [{ "Proto": proto, "Port": [dst_port] }],
+                "Node": [
+                        {
+                                "Name": aclient_name,
+                                "Tags": ["Honeypot"],
+                                "SW": ["Uchoweb"],
+                        }
+                ],
+                "Attach": [{ "request": data, "smart": data["requestline"] }]
+        }
+
+        event = w3u.IDEA_fill_addresses(event, src_ip, dst_ip, aanonymised, aanonymised_net)
+
+        return event
+
+events = []
+for line in w3u.Pygtail(filename=aconfig.get('logfile'), wait_timeout=0):
+	data = json.loads(line)
+
+	a = gen_event_idea_uchoweb(
+		detect_time = data['detect_time'], 
+		src_ip      = data['src_ip'],
+		src_port    = data['src_port'], 
+		dst_ip      = data['dst_ip'],
+		dst_port    = data['dst_port'],
+		proto       = data['proto'],
+		data        = data['data'],	
+	)
+	
+	events.append(a)
+
+print "=== Sending ==="
+start = time()
+ret = wclient.sendEvents(events)
+
+if 'saved' in ret:
+	wclient.logger.info("%d event(s) successfully delivered." % ret['saved'])
+
+print "Time: %f" % (time() - start)
+
--- puppet/hpucho/files/uchoweb/uchoweb.py
+++ puppet/hpucho/files/uchoweb/uchoweb.py
@@ -1,86 +1,24 @@
 #!/usr/bin/python
 
-#warden
-from warden_client import Client, Error, read_cfg, format_timestamp
+from warden_client import read_cfg, format_timestamp
+from jinja2 import Environment, FileSystemLoader
+import SimpleHTTPServer, SocketServer, logging, sys
 import json
-import string
-from time import time, gmtime, strftime
-from math import trunc
-from uuid import uuid4
 import os
 import sys
+import re
+import base64
+import w3utils_flab as w3u
+import mimetypes
 
-
-DEFAULT_ACONFIG = 'warden_client-uchoweb.cfg'
-DEFAULT_WCONFIG = 'warden_client.cfg'
-DEFAULT_NAME = 'org.example.warden.test'
-DEFAULT_AWIN = 5
-DEFAULT_ANONYMISED = 'no'
-DEFAULT_TARGET_NET = '0.0.0.0/0'
-
-#warden client startup
-aconfig = read_cfg(DEFAULT_ACONFIG)
-wconfig = read_cfg(aconfig.get('warden', DEFAULT_WCONFIG))
-aname = aconfig.get('name', DEFAULT_NAME)
-awin = aconfig.get('awin', DEFAULT_AWIN) * 60
-wconfig['name'] = aname
-wclient = Client(**wconfig)   
-aanonymised = aconfig.get('anonymised', DEFAULT_ANONYMISED)
-if aanonymised not in ['no', 'yes', 'omit']:
-	wclient.logger.error("Configuration error: anonymised: '%s' - possible typo? use 'no', 'yes' or 'omit'" % aanonymised)
-	sys.exit(2)
-atargetnet  = aconfig.get('target_net', DEFAULT_TARGET_NET)
-aanonymised = aanonymised if (atargetnet != DEFAULT_TARGET_NET) or (aanonymised == 'omit') else DEFAULT_ANONYMISED
-
-def gen_event_idea_uchoweb(client_name, detect_time, conn_count, src_ip, dst_ip, anonymised, target_net, 
-	peer_proto, peer_port, uchoweb_port, data):
-
-	event = {
-		"Format": "IDEA0",
-		"ID": str(uuid4()),
-		"DetectTime": detect_time,
-		"Category": ["Other"],
-		"Note": "Uchoweb event",
-		"ConnCount": conn_count,
-		"Source": [{ "Proto": peer_proto, "Port": [peer_port] }],
-		"Target": [{ "Proto": peer_proto, "Port": [uchoweb_port] }],
-		"Node": [
-			{
-				"Name": client_name,
-				"Tags": ["Honeypot"],
-				"SW": ["Uchoweb"],
-			}
-		],
-		"Attach": [{ "request": data, "smart": data["requestline"] }]
-	}
-	event = fill_addresses(event, src_ip, anonymised, target_net)
-  
-	return event
-
-def fill_addresses(event, src_ip, anonymised, target_net):
-	af = "IP4" if not ':' in src_ip else "IP6"
-	event['Source'][0][af] = [src_ip]
-	if anonymised != 'omit':
-		if anonymised == 'yes':
-			event['Target'][0]['Anonymised'] = True
-			event['Target'][0][af] = [target_net]
-		else:
-			event['Target'][0][af] = [dst_ip]
-	return event
-
-
-
-
-#uchoweb2
-import SimpleHTTPServer, SocketServer, logging, sys
-import re, os, mimetypes, base64
-from jinja2 import Environment, FileSystemLoader
+hconfig = read_cfg('uchoweb.cfg')
 
 content_base = os.path.join(os.getcwd(), "content/")
 templates_base = os.path.join(os.getcwd(), "templates/")
-port = aconfig.get('port', 8081)
-personality = aconfig.get('personality', 'Apache Tomcat/7.0.56')
+port = hconfig.get('port', 8081)
+personality = hconfig.get('personality', 'Apache Tomcat/7.0.56')
 
+logger = w3u.getLogger(hconfig['logfile'])
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	server_version = personality
@@ -96,41 +34,24 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
 	def doRespond(self):
 
-		#log
-        	wclient.logger.info("=======")
-	        wclient.logger.info(self.__dict__)
-	        wclient.logger.info(str(self.headers))
 		body_len = int(self.headers.getheader('content-length', 0))
 		body_data = self.rfile.read(body_len)
-	        wclient.logger.info(body_data)
-        	wclient.logger.info("=======")
+	
+		data2log = {
+			"detect_time" : format_timestamp(),
+			"src_ip"      : self.client_address[0],
+			"src_port"    : self.client_address[1],
+			"dst_ip"      : self.request.getsockname()[0],
+			"dst_port"    : port,
+			"proto"       : ["tcp", "http"],
+			"data"        : {"requestline": self.requestline, 
+			  	         "headers"    : str(self.headers), 
+				         "body"       : base64.b64encode(body_data), 
+				         "body_len"   : body_len }
+		}
 		
-		#import pdb; pdb.set_trace()
-
-		#report
-		a = gen_event_idea_uchoweb(
-			client_name = aname, 
-			detect_time = format_timestamp(),
-			conn_count = 1, 
-			anonymised = aanonymised, 
-			target_net = atargetnet,
-	
-			peer_proto = ["tcp", "http"],
-	
-			peer_port = self.client_address[1],
-			src_ip = self.client_address[0],
-	
-			uchoweb_port = port,
-			dst_ip = "0.0.0.0",
-	
-			data = { "requestline": self.requestline, "headers": str(self.headers), "body": base64.b64encode( body_data ), "body_len": body_len }
-		)
-		wclient.logger.debug("event %s" % json.dumps(a, indent=2))
-		ret = wclient.sendEvents([a])
-		if 'saved' in ret:
-			wclient.logger.info("%d event(s) successfully delivered." % ret['saved'])
-
-
+		logger.info(json.dumps(data2log))
+		
 		#process request
 		if self.path.endswith('/'):
 			file_name = self.path+'index.html'
@@ -151,7 +72,6 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			f.close()
 		
 		except Exception as e:
-			wclient.logger.warning(e)
 			return self.doAbort()
 
 	        self.send_response(200)
@@ -168,7 +88,6 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
 
 SocketServer.TCPServer.allow_reuse_address = True
-httpd = SocketServer.TCPServer(("0.0.0.0", port), ServerHandler)
-wclient.logger.info("starting server")
+httpd = SocketServer.TCPServer(("", port), ServerHandler)
 httpd.serve_forever()
 
