f3cce38c4c42d6297e017b55ab57a9b9547dc555
Michal Kostenec <kostenec@cesnet.cz>
2015-11-09 14:02:41 +0100

    telnetd lib path fix

--- puppet/hptelnetd/tests/hptelnetd.sh
+++ puppet/hptelnetd/tests/hptelnetd.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-#. /puppet/metalib/lib.sh
-
-netstat -vlanp | grep -E "0.0.0.0:23\s+"
-
-if [ $? -ne 0 ]; then
-        rreturn 1 "$0 failed to start telnetd"
-fi      
+#. /puppet/metalib/bin/lib.sh
 
 python -c 'print "autotest\nautotest.123456\nid\nexit"' | timeout 2s nc $(facter ipaddress) 23 | grep 'uid=0'
 
 if [ $? -ne 0 ]; then
-        rreturn 2 "$0 failed to login to telnetd"
+        rreturn 2 "$0 failed to login to hptelnetd"
 fi 
 
 rreturn 0 "$0"
