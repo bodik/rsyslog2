0fbde42eb9a719e2023c1ddcaf2c727e4bfcbba8
root <root@debian.domain.cz>
2016-02-15 16:04:44 +0100

    portfix

--- puppet/hpjdwpd/tests/hpjdwpd.sh
+++ puppet/hpjdwpd/tests/hpjdwpd.sh
@@ -2,7 +2,7 @@
 
 . /puppet/metalib/bin/lib.sh
 
-python -c 'print "JDWP-Handshake\x00\x00\x00\x14\x00\x00\x00\x01\x00\x0f\x0f\x41\x55\x54\x4f\x54\x45\x53\x54"' | nc localhost 58000 | grep "AUTOTEST"
+python -c 'print "JDWP-Handshake\x00\x00\x00\x14\x00\x00\x00\x01\x00\x0f\x0f\x41\x55\x54\x4f\x54\x45\x53\x54"' | nc localhost 8000 | grep "AUTOTEST"
 
 if [ $? -ne 0 ]; then
         rreturn 2 "$0 failed to jdwpd handshake"
