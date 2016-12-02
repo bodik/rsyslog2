b9315f9ef11a7cbce551fba780359558890d3e38
root <root@gw-esb4.meta.zcu.cz>
2016-12-02 16:30:28 +0100

    esd currator name refactoring

--- puppet/elk/bin/delete-older-indexes.sh
+++ puppet/elk/bin/delete-older-indexes.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# deletes indexes older int()
+#
+# cronline
+# 0 1 * * * /bin/sh /puppet/elk/bin/delete-older-indexes.sh 14
 
 if [ -z $1 ]; then
 	echo "ERROR: no horizont specified"
@@ -8,7 +12,7 @@ TODAY=$(date +"%s")
 HORIZONT=$(( $TODAY - 3600*24*$1 ))
 ESD=$(facter ipaddress_eth1 ipaddress_eth0 | sort -r | awk '{print $3}' | tr '\n' ' ' | awk '{print $1}')
 
-for all in $(sh /puppet/elk/bin/elk-listindexes.sh | grep logstash | awk '{print $3}'); do
+for all in $(sh /puppet/elk/bin/listindexes.sh | grep logstash | awk '{print $3}'); do
 	TMP=$(date -d $(echo $all | sed 's/logstash\-//' | sed 's/\./\-/g') +"%s")
 	if [ $TMP -lt $HORIZONT ]; then
 		#echo "$all to be deleted"
